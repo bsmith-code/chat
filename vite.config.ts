d40e51f2e2c82e01b88c78b727445daa3c2bf254
Brian M. Smith <brian@brianmatthewsmith.com>
2023-12-14 13:12:09 -0500

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   server: {
     port: 3003,
     host: 'chat.brianmatthewsmith.local'
